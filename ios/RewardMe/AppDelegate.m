b5dcd1b3c9a22989c7c6ff047a713c91b615c501
Gem Ng <gem@mailtime.com>
2020-04-20 17:35:47 +0800

    Rename the project.

--- ios/RewardMe/AppDelegate.m
+++ ios/RewardMe/AppDelegate.m
@@ -48,7 +48,7 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"mymdtmobile"
+                                                   moduleName:@"RewardMe"
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
