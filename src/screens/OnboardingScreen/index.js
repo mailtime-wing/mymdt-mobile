554363ea84d7e8209525901e57acdcfa35ef8042
wing <wing@mailtime.com>
2020-04-22 17:20:36 +0800

    refactor onboarding screen and splash screen

--- src/screens/OnboardingScreen/index.js
+++ src/screens/OnboardingScreen/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
 import {
   Container,
   ContentContainer,
@@ -6,30 +7,117 @@ import {
   ColorBackground,
   Header,
   Details,
-  Skip,
+  SwiperContainer,
+  MarginContainer,
 } from './style';
-import Button from '@/components/Button';
+import ThemeButton from '@/components/ThemeButton';
 import {FormattedMessage} from 'react-intl';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
+
+const {width: viewportWidth} = Dimensions.get('window');
+
+const wp = percentage => {
+  const value = (percentage * viewportWidth) / 100;
+  return Math.round(value);
+};
+
+const slideWidth = wp(75);
+const itemHorizontalMargin = wp(2);
+
+const sliderWidth = viewportWidth;
+const itemWidth = slideWidth + itemHorizontalMargin * 2;
+
+const renderItem = ({item, index}) => {
+  return (
+    <ContentContainer>
+      <ColorBackground backgroundColor="#DFF7FF" />
+      <Header>{item.header}</Header>
+      <Details>{item.detail}</Details>
+    </ContentContainer>
+  );
+};
+
+const CardSection = () => {
+  const [activeIndex, setActiveIndex] = useState(1);
+  const refCarousel = useRef(null);
+
+  const data = [
+    {
+      header: 'Login to your email. Shop. Get cashback rewards.',
+      detail:
+        'Select your favorite brands and start shopping to earn cashback rewards.',
+    },
+    {
+      header: 'Earn Points every time you shop your favorite brands.',
+      detail:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, (Need Big tech terms to distract users)',
+    },
+    {
+      header: 'Collect Points and redeem them on Gift Cards.',
+      detail:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy ',
+    },
+  ];
+
+  return (
+    <SwiperContainer>
+      <Carousel
+        ref={refCarousel}
+        layout={'default'}
+        data={data}
+        renderItem={renderItem}
+        sliderWidth={sliderWidth}
+        itemWidth={itemWidth}
+        inactiveSlideScale={1}
+        inactiveSlideOpacity={0}
+        containerCustomStyle={styles.container}
+        activeAnimationType={'decay'}
+        onSnapToItem={index => setActiveIndex(index)}
+      />
+      <Pagination
+        dotsLength={data.length}
+        activeDotIndex={activeIndex}
+        containerStyle={styles.paginationContainer}
+        dotColor={'#00BACE'}
+        dotStyle={styles.paginationDot}
+        inactiveDotColor={'rgba(3, 99, 239, 0.2)'}
+        inactiveDotScale={1}
+        carouselRef={refCarousel}
+        tappableDots={!!refCarousel}
+      />
+    </SwiperContainer>
+  );
+};
 
 const OnboardingScreen = ({navigation}) => (
   <Container>
-    <ContentContainer>
-      <ColorBackground backgroundColor="#DFF7FF" />
-      <Header>Collect MDT and redeem them on Gift Cards.</Header>
-      <Details>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-        nonummy
-      </Details>
-    </ContentContainer>
+    <CardSection />
     <ButtonContainer>
-      <Button onPress={() => navigation.navigate('brand_select')}>
-        <FormattedMessage id="start_earning_money" />
-      </Button>
-      <Skip onPress={() => navigation.navigate('sign_in', {isSignUp: false})}>
-        <FormattedMessage id="already_have_account" />
-      </Skip>
+      <ThemeButton onPress={() => navigation.navigate('brand_select')}>
+        <FormattedMessage id="join_rewardme" defaultMessage="JOIN REWARDME" />
+      </ThemeButton>
+      <MarginContainer />
+      <ThemeButton
+        reverse
+        small
+        onPress={() => navigation.navigate('sign_in', {isSignUp: false})}>
+        <FormattedMessage id="sign_in" defaultMessage="Sign In" />
+      </ThemeButton>
     </ButtonContainer>
   </Container>
 );
 
 export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {overflow: 'visible'},
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 8,
+  },
+  paginationContainer: {
+    marginVertical: 30,
+  },
+});
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,9 @@
 const theme = {
   colors: {
+    primary: ['#0363EF', '#0036C5'],
+    secondary: ['#B5E8EE', '#21CEDB', '#3DB4C8'],
+    error: '#E95959',
+    alert: '#FFD542',
     grey: ['#EEEEEE', '#DFDFDF', '#C6C6C6', '#AAAAAA', '#7B7B7B', '#797979'],
     white: ['#FFFFFF', '#F2F2F2'],
     black: ['rgba(0, 0, 0, 0.17)', 'rgba(0, 0, 0, 0.4)', '#000000'],
@@ -18,5 +22,11 @@ theme.colors.white.dark = theme.colors.white[1];
 theme.colors.black.superLight = theme.colors.black[0];
 theme.colors.black.light = theme.colors.black[1];
 theme.colors.black.normal = theme.colors.black[2];
+
+theme.colors.primary.normal = theme.colors.primary[0];
+theme.colors.primary.dark = theme.colors.primary[1];
+theme.colors.secondary.light = theme.colors.secondary[0];
+theme.colors.secondary.normal = theme.colors.secondary[1];
+theme.colors.secondary.dark = theme.colors.secondary[2];
 
 export default theme;
