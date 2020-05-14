c6a30cf11a8253761ca1c3dfe7c841140153d38c
Maxi Cho <maxi@mailtime.com>
2020-05-14 17:36:39 +0800

    add polyfill for `Intl` which comes from `react-intl` so that the app can run on Android

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * @format
  */
 
+import '@/polyfills';
+
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
---
+++ src/polyfills.js
@@ -0,0 +1,15 @@
+/**
+ * This is polyfill required by `Intl` (which comes from `react-intl`)
+ * TODO: check the following
+ * 1. zh-Hant and zh-Hans are required
+ * 2. all polyfills required
+ *
+ * references:
+ * https://github.com/andyearnshaw/Intl.js
+ * https://formatjs.io/docs/react-intl/#react-native
+ * https://formatjs.io/docs/react-intl/#runtime-requirements
+ */
+if (!global.Intl) {
+  global.Intl = require('intl');
+  require('intl/locale-data/jsonp/en');
+}
