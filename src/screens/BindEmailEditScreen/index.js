41d12d5fbced8b1de3b3a8408f6db843563bb857
wing <wing@mailtime.com>
2020-05-21 16:17:10 +0800

    update email binding edit screen ui

--- src/screens/BindEmailEditScreen/index.js
+++ src/screens/BindEmailEditScreen/index.js
@@ -1,11 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useLayoutEffect, useReducer} from 'react';
 import {FormattedMessage} from 'react-intl';
-import {Container, ButtonContainer, EmailText} from './style';
+import {
+  Container,
+  ButtonContainer,
+  EmailText,
+  UnbindButton,
+  UnbindText,
+  TitleContainer,
+  TitleText
+} from './style';
 
 import ModalContainer from '@/components/ModalContainer';
 import SpecialListOption from '@/components/SpecialListOption';
 import ThemeButton from '@/components/ThemeButton';
 import Switch from '@/components/Switch';
+import EditButton from '@/components/EditButton';
+import CancelButton from '@/components/CancelButton';
+import ConfirmButton from '@/components/ConfirmButton';
+import HeaderButton from '@/components/HeaderButton';
+import PopupModal from '@/components/PopupModal';
 
 const emailsSampleData = [
   {email: 'foo@gmail.com', share: true},
@@ -13,10 +26,112 @@ const emailsSampleData = [
   {email: 'foo.bar@mailtime.com', share: false},
 ];
 
-const BindEmailEditScreen = () => {
+const UPDATE_IS_EDITING = 'updateIsEditing';
+const UPDATE_IS_CONFIRMED = 'updateIsConfirmed';
+const UPDATE_IS_CANCELLED = 'updateIsCancelled';
+const UPDATE_IS_EMAIL_UNBINDING = 'updateIsEmailUnbinding';
+const UPDATE_IS_EMAIL_UNBIND_CANCELLED = 'updateIsEmailUnbindCancelled';
+const UPDATE_IS_EMAIL_UNBIND_CONFIRMED = 'updateIsEmailUnbindConfirmed';
+
+const initialState = {
+  isEditing: false,
+  isUnbinding: false,
+  isConfirmed: false,
+  unbindingEmail: ''
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case UPDATE_IS_CANCELLED: {
+      return initialState
+    }
+    case UPDATE_IS_EDITING: {
+      return {
+        ...state,
+        isEditing: true
+      }
+    }
+    case UPDATE_IS_CONFIRMED: {
+      return {
+        ...state,
+        // no action this moment
+      }
+    }
+    case UPDATE_IS_EMAIL_UNBINDING: {
+      return {
+        ...state,
+        isUnbinding: true,
+        isConfirmed: false,
+        unbindingEmail: action.payload
+      }
+    }
+    case UPDATE_IS_EMAIL_UNBIND_CANCELLED: {
+      return {
+        ...state,
+        isUnbinding: false,
+        unbindingEmail: ''
+      }
+    }
+    case UPDATE_IS_EMAIL_UNBIND_CONFIRMED: {
+      return {
+        ...state,
+        isEditing: false,
+        isUnbinding: false,
+        unbindingEmail: '',
+      }
+    }
+    default:
+      throw new Error();
+  }
+}
+
+const BindEmailEditScreen = ({navigation}) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
   const [emails, setEmails] = useState(
     emailsSampleData.map(email => email.share),
   );
+
+  console.log(state)
+
+  useLayoutEffect(() => {
+    if (state.isEditing) {
+      navigation.setOptions({
+        headerLeft: () => (
+          <CancelButton onPress={handleCancelPress} />
+        ),
+        headerRight: () => <ConfirmButton onPress={handleConfirmPress} />,
+      });
+    } else {
+      navigation.setOptions({
+        headerLeft: () => <HeaderButton isModal root="menu" />,
+        headerRight: () => <EditButton onPress={handleEditPress} />,
+      });
+    }
+  }, [state.isEditing, navigation]);
+
+  const handleEditPress = () => {
+    dispatch({ type: UPDATE_IS_EDITING })
+  };
+
+  const handleCancelPress = () => {
+    dispatch({ type: UPDATE_IS_CANCELLED })
+  }
+
+  const handleConfirmPress = () => {
+    // no action this moment
+  };
+
+  const handleUnbindPress = email => {
+    dispatch({ type: UPDATE_IS_EMAIL_UNBINDING, payload: email })
+  };
+
+  const handleCallback = result => {
+    if (result === 'OK') {
+      dispatch({ type: UPDATE_IS_EMAIL_UNBIND_CONFIRMED })
+      return
+    }
+    dispatch({ type: UPDATE_IS_EMAIL_UNBIND_CANCELLED })
+  };
 
   const handleInputChange = index => {
     const data = [...emails];
@@ -25,27 +140,50 @@ const BindEmailEditScreen = () => {
   };
 
   return (
-    <ModalContainer title={<FormattedMessage id="emails_binding" />}>
+    <ModalContaienr title={<FormattedMessage id="emails_manage" />}>
+      <TitleContainer>
+        <TitleText>
+          <FormattedMessage id="email" defaultMessage="email" />
+        </TitleText>
+        {!state.isEditing && 
+          <TitleText>
+            <FormattedMessage id="data_sharing" defaultMessage="data sharing" />
+          </TitleText>
+        }
+      </TitleContainer>
       <Container>
         {emails.map((email, index) => (
           <SpecialListOption
             key={email.emailAddress}
             label={<EmailText>{email.emailAddress}</EmailText>}
             value={
-              <Switch
-                value={emails[index]}
-                onChange={() => handleInputChange(index)}
-              />
+              state.isEditing ? (
+                <UnbindButton onPress={() => handleUnbindPress(email.email)}>
+                  <UnbindText><FormattedMessage id="unbind" defaultMessage="unbind" /></UnbindText>
+                </UnbindButton>
+              ) : (
+                <Switch
+                  value={emails[index]}
+                  onChange={() => handleInputChange(index)}
+                />
+              )
             }
           />
         ))}
       </Container>
-      <ButtonContainer>
-        <ThemeButton small>
-          <FormattedMessage id="add_email_account" />
+      {state.isEditing && <ButtonContainer>
+        <ThemeButton onPress={() => navigation.navigate('emails_binding', {navigateFromEdit: true})} small width="auto">
+          <FormattedMessage id="add_email_account" defaultMessage="ADD EMAIL" />
         </ThemeButton>
-      </ButtonContainer>
-    </ModalContainer>
+      </ButtonContainer>}
+      {state.isUnbinding && (
+        <PopupModal
+          title="Unbind this email"
+          detail={`You will not get any data rewards from ${state.unbindingEmail} after unbinding.`}
+          callback={handleCallback}
+        />
+      )}
+    </ModalContaienr>
   );
 };
 
--- src/screens/BindEmailEditScreen/style.js
+++ src/screens/BindEmailEditScreen/style.js
@@ -1,7 +1,25 @@
 import styled from '@emotion/native';
 import Text from '@/components/AppText';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding-left: 24px;
+  padding-right: 24px;
+`;
+
+export const TitleContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 16px 24px;
+  align-items: center;
+`;
+
+export const TitleText = styled(Text)`
+  font-weight: bold;
+  font-size: 12px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: ${props => props.theme.colors.black.superLight};
+`;
 
 export const CustomeSwitch = styled.Switch`
   border: 2px solid black;
@@ -17,4 +35,18 @@ export const EmailText = styled(Text)`
   font-size: 16px;
   line-height: 16px;
   letter-spacing: 0.75px;
+`;
+
+export const UnbindButton = styled.TouchableOpacity`
+  border: 1px solid ${props => props.theme.colors.error.superLight};
+  border-radius: 20px;
+  padding: 6px 8px;
+  height: auto;
+`;
+
+export const UnbindText = styled.Text`
+  font-size: 14px;
+  line-height: 17px;
+  font-weight: bold;
+  color: ${props => props.theme.colors.error.dark};
 `;
