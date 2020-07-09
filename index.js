d6fec0b6cbb8e4189f8b557ca581a1deea9840e3
wing <wing@mailtime.com>
2020-07-09 18:58:18 +0800

    change ignoreWarnings to 'MailtimeAuth'

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ import {AppRegistry, YellowBox} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-YellowBox.ignoreWarnings(['Module']);
+YellowBox.ignoreWarnings(['MailtimeAuth']);
 AppRegistry.registerComponent(appName, () => App);
