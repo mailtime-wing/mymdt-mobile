e1f040758c18f5ef01e68a71bba5f8709b37b979
wing <wing@mailtime.com>
2020-07-15 11:45:43 +0800

    comment fix

--- src/components/MembershipLevelChip/index.js
+++ src/components/MembershipLevelChip/index.js
@@ -3,43 +3,43 @@ import {LevelChip, LevelText} from './style';
 import membershipLevel from '@/enum/membershipLevel';
 import {FormattedMessage} from 'react-intl';
 
-const MembershipLevelChip = ({userLevel, style}) => {
-  const levels = [
-    {
-      level: membershipLevel.BASIC,
-      backgroundColor: props => props.theme.colors.membership.basic.background,
-      textColor: props => props.theme.colors.membership.basic.text,
-    },
-    {
-      level: membershipLevel.SILVER,
-      backgroundColor: props => props.theme.colors.membership.silver.background,
-      textColor: props => props.theme.colors.membership.silver.text,
-    },
-    {
-      level: membershipLevel.GOLD,
-      backgroundColor: props => props.theme.colors.membership.gold.background,
-      textColor: props => props.theme.colors.membership.gold.text,
-    },
-    {
-      level: membershipLevel.PLATINUM,
-      backgroundColor: props =>
-        props.theme.colors.membership.platinum.background,
-      textColor: props => props.theme.colors.membership.platinum.text,
-    },
-    {
-      level: membershipLevel.DIAMOND,
-      backgroundColor: props =>
-        props.theme.colors.membership.diamond.background,
-      textColor: props => props.theme.colors.membership.diamond.text,
-    },
-    {
-      level: membershipLevel.SPECIAL,
-      backgroundColor: props =>
-        props.theme.colors.membership.special.background,
-      textColor: props => props.theme.colors.membership.special.text,
-    },
-  ];
+const levels = [
+  {
+    level: membershipLevel.BASIC,
+    backgroundColor: props => props.theme.colors.membership.basic.background,
+    textColor: props => props.theme.colors.membership.basic.text,
+  },
+  {
+    level: membershipLevel.SILVER,
+    backgroundColor: props => props.theme.colors.membership.silver.background,
+    textColor: props => props.theme.colors.membership.silver.text,
+  },
+  {
+    level: membershipLevel.GOLD,
+    backgroundColor: props => props.theme.colors.membership.gold.background,
+    textColor: props => props.theme.colors.membership.gold.text,
+  },
+  {
+    level: membershipLevel.PLATINUM,
+    backgroundColor: props =>
+      props.theme.colors.membership.platinum.background,
+    textColor: props => props.theme.colors.membership.platinum.text,
+  },
+  {
+    level: membershipLevel.DIAMOND,
+    backgroundColor: props =>
+      props.theme.colors.membership.diamond.background,
+    textColor: props => props.theme.colors.membership.diamond.text,
+  },
+  {
+    level: membershipLevel.SPECIAL,
+    backgroundColor: props =>
+      props.theme.colors.membership.special.background,
+    textColor: props => props.theme.colors.membership.special.text,
+  },
+];
 
+const MembershipLevelChip = ({userLevel, style}) => {
   const currentLevel = levels.find(level => level.level === userLevel);
 
   return (
