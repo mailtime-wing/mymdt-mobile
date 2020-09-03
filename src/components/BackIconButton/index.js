aed54291d92c5b8de1556d9c7d536ce27116155f
wing <wing@mailtime.com>
2020-09-03 19:03:46 +0800

    use theme value instead of hardcode

--- src/components/BackIconButton/index.js
+++ src/components/BackIconButton/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
+import {useTheme} from 'emotion-theming';
 import IconButton from '@/components/IconButton';
 import BackIcon from '@/assets/icon_chevron-left.svg';
 
-const BackIconButton = props => (
-  <IconButton
-    variant="outlined"
-    sizeVariant="small"
-    color="black"
-    svgIcon={BackIcon}
-    {...props}
-  />
-);
+const BackIconButton = props => {
+  const theme = useTheme();
+  return (
+    <IconButton
+      variant="outlined"
+      sizeVariant="small"
+      color={theme.colors.contrastColor}
+      svgIcon={BackIcon}
+      {...props}
+    />
+  );
+};
 
 export default BackIconButton;
