6704bd8ba7ecc086acca6f2e533ee5ec815dfd16
wing <wing@mailtime.com>
2020-09-16 17:07:58 +0800

    add <MembershipCard />

---
+++ src/components/MembershipCard/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+import {image} from './style';
+
+import membershipLevel from '@/enum/membershipLevel';
+
+const levelCardList = [
+  {
+    level: membershipLevel.BASIC,
+    cardSrc: require('@/assets/basic_membership_card.png'),
+  },
+  {
+    level: membershipLevel.SILVER,
+    cardSrc: require('@/assets/silver_membership_card.png'),
+  },
+  {
+    level: membershipLevel.GOLD,
+    cardSrc: require('@/assets/gold_membership_card.png'),
+  },
+  {
+    level: membershipLevel.PLATINUM,
+    cardSrc: require('@/assets/platinum_membership_card.png'),
+  },
+  {
+    level: membershipLevel.DIAMOND,
+    cardSrc: require('@/assets/diamond_membership_card.png'),
+  },
+  {
+    level: membershipLevel.SPECIAL,
+    cardSrc: require('@/assets/special_membership_card.png'),
+  },
+];
+
+const MembershipCard = ({userLevel, style}) => {
+  return (
+    <Image
+      source={
+        levelCardList.find(levelCard => userLevel === levelCard.level).cardSrc
+      }
+      style={[image, style]}
+    />
+  );
+};
+
+export default MembershipCard;
---
+++ src/components/MembershipCard/style.js
@@ -0,0 +1,7 @@
+import {css} from '@emotion/native';
+
+export const image = css`
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 12px;
+`;
