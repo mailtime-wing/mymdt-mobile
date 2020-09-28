946ea668f2c4fc1feef9ba6ca4eb99c353bd1a72
Maxi Cho <maxi@mailtime.com>
2020-09-28 18:37:54 +0800

    fix warning

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 
 import '@/polyfills';
 
-import {AppRegistry, YellowBox} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-YellowBox.ignoreWarnings(['MailtimeAuth']);
+LogBox.ignoreLogs(['MailtimeAuth']);
 AppRegistry.registerComponent(appName, () => App);
