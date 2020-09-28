35162dac591940e067f4332adaf1937847a4e74d
wing <wing@mailtime.com>
2020-09-28 18:05:43 +0800

    update onboarding ui

--- src/screens/OnboardingScreen/style.js
+++ src/screens/OnboardingScreen/style.js
@@ -1,46 +1,29 @@
-import styled, {css} from '@emotion/native';
+import {css} from '@emotion/native';
 import {StyleSheet} from 'react-native';
 
-export const Container = styled.View`
-  padding-top: 76px;
+export const container = css`
   padding-bottom: 24px;
 `;
 
-export const ContentContainer = styled.View`
-  padding-left: 24px;
-  padding-right: 24px;
+export const paddingHorizontal = css`
+  padding-horizontal: 24px;
 `;
 
-export const ButtonContainer = styled.View`
-  padding-left: 24px;
-  padding-right: 24px;
-`;
-
-export const ColorBackground = styled.View`
-  color: ${props => props.backgroundColor};
-  aspect-ratio: 1;
-  border-radius: 24px;
-  margin-left: 16px;
-  margin-right: 16px;
-`;
-
-export const headerStyle = theme => css`
+export const headerStyle = (theme) => css`
   margin-top: 40px;
   margin-bottom: 10px;
   color: ${theme.colors.secondary.normal};
   text-align: center;
 `;
 
-export const detailStyle = theme => css`
+export const detailStyle = (theme) => css`
   color: ${theme.colors.textOnBackground.mediumEmphasis};
   text-align: center;
 `;
 
-export const MarginContainer = styled.View`
-  margin-top: 12px;
+export const image = css`
+  width: 100%;
 `;
-
-export const SwiperContainer = styled.View``;
 
 export const styles = StyleSheet.create({
   container: {
@@ -51,7 +34,10 @@ export const styles = StyleSheet.create({
     width: 8,
     height: 8,
     borderRadius: 4,
-    marginHorizontal: 8,
   },
-  paginationContainer: {},
+  paginationContainer: {
+    paddingVertical: 0,
+    marginTop: 60,
+    marginBottom: 16,
+  },
 });
