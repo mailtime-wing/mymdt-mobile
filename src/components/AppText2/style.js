11f50b4deb57ca385e7ade11becfa22d37232641
wing <wing@mailtime.com>
2020-10-15 14:47:52 +0800

    add font style for new variant in Typography(digit12mono, digit16mono, digit36, digit36mono)

--- src/components/AppText2/style.js
+++ src/components/AppText2/style.js
@@ -7,14 +7,22 @@ export const text = (theme, variant) => css`
   line-height: ${String(theme.lineHeights[variant])}px;
 
   ${variant === 'pageTitle' &&
-    `
+  `
     text-transform: uppercase;
   `};
   ${variant === 'button' &&
-    'text-transform: uppercase; letter-spacing: 1.5px;'};
+  'text-transform: uppercase; letter-spacing: 1.5px;'};
   ${variant === 'label' && 'text-transform: uppercase;'};
   ${variant === 'overline' && 'text-transform: uppercase;'};
   ${variant === 'initials1' && 'text-transform: uppercase;'};
   ${variant === 'initials2' && 'text-transform: uppercase;'};
   ${variant === 'initials3' && 'text-transform: uppercase;'};
+  ${variant === 'digit12mono' &&
+  "font-feature-settings: 'tnum' on, 'lnum' on, 'ss02' on, 'cv01' on, 'cv03' on, 'cv04' on, 'cv02' on;"};
+  ${variant === 'digit16mono' &&
+  "font-feature-settings: 'tnum' on, 'lnum' on, 'ss02' on, 'cv01' on, 'cv03' on, 'cv04' on, 'cv02' on;"};
+  ${variant === 'digit36' &&
+  "font-feature-settings: 'pnum' on, 'lnum' on, 'ss02' on, 'cv01' on, 'cv02' on, 'cv03' on, 'cv04' on, 'zero' on;"};
+  ${variant === 'digit36mono' &&
+  "font-feature-settings: 'tnum' on, 'lnum' on, 'ss02' on, 'cv01' on, 'cv03' on, 'cv04' on, 'cv02' on;"};
 `;
