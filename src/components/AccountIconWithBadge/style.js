f3e1e976ea386c7bcc6714bc27273f834719f114
wing <wing@mailtime.com>
2020-11-05 15:54:08 +0800

    add <AccountIconWithBadge />

---
+++ src/components/AccountIconWithBadge/style.js
@@ -0,0 +1,11 @@
+import {css} from '@emotion/native';
+
+export const membershipPosition = css`
+  position: absolute;
+  left: 30;
+  bottom: 0;
+`;
+
+export const container = css`
+  flex-direction: row;
+`;
