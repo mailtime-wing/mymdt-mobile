6cb467a75cf375336a5b28814a70e47782749bf9
wing <wing@mailtime.com>
2020-11-05 18:22:56 +0800

    make <AppAvator /> support svgIcon, custom color and backgroundColor

--- src/components/AppAvator/style.js
+++ src/components/AppAvator/style.js
@@ -1,47 +1,50 @@
 import {css} from '@emotion/native';
 import {StyleSheet} from 'react-native';
 
-export const container = sizeVariant => css`
+export const container = (sizeVariant) => css`
   border-radius: 36px;
   ${sizeVariant === 'small' &&
-    `
+  `
     width: 40px;
     height: 40px;
     `}
   ${sizeVariant === 'normal' &&
-    `
+  `
     width: 56px;
     height: 56px;
     `}
   ${sizeVariant === 'large' &&
-    `
+  `
     width: 72px;
     height: 72px;
   `}
 `;
 
-export const icon = sizeVariant => css`
+export const icon = (sizeVariant, color) => css`
+  stroke-width: 2px;
+  fill: ${color};
+  stroke: ${color};
+
   ${sizeVariant === 'small' &&
-    `
-    width: 18px;
-    height: 18px;
+  `
+    width: 24px;
+    height: 24px;
     `}
   ${sizeVariant === 'normal' &&
-    `
+  `
     width: 24px;
     height: 24px;
     `}
   ${sizeVariant === 'large' &&
-    `
+  `
     width: 40px;
     height: 40px;
   `}
 `;
 
-export const iconContainer = theme => css`
+export const iconContainer = css`
   justify-content: center;
   align-items: center;
-  background-color: ${theme.colors.secondary.normal};
 `;
 
 export const initialsContainer = (theme, colorIndex) => css`
@@ -50,11 +53,11 @@ export const initialsContainer = (theme, colorIndex) => css`
   background-color: ${theme.colors.avatarsInitials[colorIndex]};
 `;
 
-export const initials = theme => css`
+export const initials = (theme) => css`
   color: ${theme.colors.background1};
 `;
 
-export const styles = theme =>
+export const styles = (theme) =>
   StyleSheet.create({
     // passing border-related styles to <Image /> using emotion cause warning. Use native StyleSheet instead
     border: {
