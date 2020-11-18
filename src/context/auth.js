dd592180c4c9130c6c0135997cd9e94760165d81
Maxi Cho <maxi@mailtime.com>
2020-11-18 11:10:28 +0800

    fix bug that app stays in splash screen if refresh token is invalid or expired

--- src/context/auth.js
+++ src/context/auth.js
@@ -112,7 +112,6 @@ export const AuthProvider = ({children}) => {
             } catch (e) {
               authToken = '';
               refreshToken = '';
-              return;
             }
           }
         }
