1940314ba6b8f5072f54f6f57ff222053efb4f8e
wing <wing@mailtime.com>
2020-12-18 12:42:11 +0800

    makesure background flash cover membership card

--- src/screens/UpgradeScreen/style.js
+++ src/screens/UpgradeScreen/style.js
@@ -4,7 +4,7 @@ export const container = css`
   margin-top: auto;
   margin-bottom: auto;
   align-self: center;
-  z-index: 1;
+  z-index: 0;
 `;
 
 export const congrats = (theme) => css`
@@ -23,7 +23,7 @@ export const button = css`
 `;
 
 export const firework = css`
-  z-index: 0;
+  z-index: -1;
   position: absolute;
   top: 0;
   bottom: 0;
