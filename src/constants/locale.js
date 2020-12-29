08f2381d80a9faebbb757be1688050b43b919f95
wing <wing@mailtime.com>
2020-12-29 17:23:42 +0800

    add translations for vi, th, fil, my and br

--- src/constants/locale.js
+++ src/constants/locale.js
@@ -2,6 +2,11 @@ const locales = {
   EN_US: 'en-US',
   ZH_HK: 'zh-HK',
   ZH_CN: 'zh-CN',
+  VI: 'vi',
+  FIL: 'fil',
+  TH: 'th',
+  MS_MY: 'ms-MY',
+  PT_BR: 'pt-BR',
 };
 
 export default locales;
