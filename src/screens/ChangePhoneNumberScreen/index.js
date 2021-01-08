ff12557c2f4b45a13ccc7f0e243c82e8fb80ba77
Cho Chi Him <chochihim@gmail.com>
2021-01-08 15:13:23 +0800

    use specific key for specific error message

--- src/screens/ChangePhoneNumberScreen/index.js
+++ src/screens/ChangePhoneNumberScreen/index.js
@@ -19,14 +19,14 @@ const renderClientError = (errorCode) => {
     case errorCodeEnum.DATA_INVALID:
       return (
         <FormattedMessage
-          id="error.error_code_202"
+          id="error.verificaion_code_invalid"
           defaultMessage="Verification Code invalid."
         />
       );
     case errorCodeEnum.DATA_EXPIRED:
       return (
         <FormattedMessage
-          id="error.error_code_203"
+          id="error.verificaion_code_invalid"
           defaultMessage="Verification Code invalid."
         />
       );
